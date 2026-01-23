c63292f0ca5cc39b4d85bb2d13c2430524d60981
Bezuhlyi Artem <bezart06@gmail.com>
2026-01-23 11:50:59 +0200

    Commit - 10

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -51,41 +51,41 @@ export default {
     data() {
         return {
             img: 1,
-            parent: '',
+            parent: { url: '' },
             email: "",
             password: ""
         }
     },
     mounted:function() {
         this.img = this.randomIntFromInterval(1,7);
-        this.parent = this.$parent.$parent;
+        this.parent = this.$root;
     },
     methods: {
         randomIntFromInterval:function (min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         },
         handleLogin() {
-            this.$parent.formData = {
+            this.parent.formData = {
                 email: this.email,
                 password: this.password
             };
 
             var self = this;
-            var data = self.$parent.toFormData(self.$parent.formData);
+            var data = self.parent.toFormData(self.parent.formData);
 
-            axios.post(self.$parent.url + "/site/login", data).then(function (response) {
+            axios.post(self.parent.url + "/site/login", data).then(function (response) {
                 if (response.data.error) {
                     if (self.$refs.msg) {
                         self.$refs.msg.alertFun(response.data.error);
                     } else {
-                        console.error("Помилка (компонент msg не знайдено):", response.data.error);
+                        console.error("Помилка:", response.data.error);
                         alert(response.data.error);
                     }
                 }
                 if (response.data.user) {
-                    self.$parent.user = response.data.user;
-                    self.$parent.page('/campaigns');
-                    window.localStorage.setItem('user', JSON.stringify(self.$parent.user));
+                    self.parent.user = response.data.user;
+                    self.parent.page('/campaigns');
+                    window.localStorage.setItem('user', JSON.stringify(self.parent.user));
                 }
             }).catch(function (error) {
                 console.log('errors : ', error);

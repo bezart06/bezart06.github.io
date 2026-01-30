9fe0b003fa95d30df108caf203633ffedfeebad8
Bezuhlyi Artem <bezart06@gmail.com>
2026-01-30 14:56:07 +0200

    Commit - 24

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,63 +1,101 @@
 <template>
     <div class="login-container">
-        <div class="login-form-wrapper">
-             <div class="form inner-form">
-                <form @submit.prevent="login()">
-                    <h1>Sign in</h1>
-                    <div class="row">
-                        <label>Email</label>
-                        <input type="email" v-model="form.email" required>
+        <Msg ref="msg"></Msg>
+
+        <div class="left-side">
+            <img :src="parent.url+'/app/views/images/Cover_'+img+'.jpg'" alt="Background">
+        </div>
+
+        <div class="right-side">
+            <div class="top-bar">
+                <span class="top-bar-text">Affiliate Sign In</span>
+                <div class="top-bar-image">
+                    <img :src="parent.url+'/app/views/images/logo.svg'" alt="Logo" />
+                </div>
+            </div>
+
+            <div class="form-wrapper">
+
+                <form @submit.prevent="handleLogin" v-if="parent.formData">
+                    <div class="input-group">
+                        <label for="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            v-model="email"
+                            required
+                        />
                     </div>
-                    <div class="row">
-                        <label>Password</label>
-                        <input type="password" v-model="form.password" required>
+
+                    <div class="input-group">
+                        <label for="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            v-model="password"
+                            required
+                            autocomplete="on"
+                        />
                     </div>
-                    <div class="row">
-                        <button class="btn">SIGN IN</button>
-                    </div>
+
+                    <button type="submit" class="btn-login">SIGN IN</button>
                 </form>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
+import Msg from '../components/Msg.vue';
 
 export default {
     name: 'Login',
+    components: {
+        Msg
+    },
     data() {
         return {
-            form: {
-                email: "",
-                password: ""
-            },
-            parent: null
+            img: 1,
+            parent: { url: '' },
+            email: "",
+            password: ""
         }
     },
-    mounted() {
+    mounted:function() {
+        this.img = this.randomIntFromInterval(1,7);
         this.parent = this.$root;
-        if(this.parent.user && this.parent.user.auth) {
-            this.$router.push('/campaigns');
-        }
     },
     methods: {
-        login() {
+        randomIntFromInterval:function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        handleLogin() {
+            this.parent.formData = {
+                email: this.email,
+                password: this.password
+            };
+
             var self = this;
-            var data = self.parent.toFormData(self.form);
+            var data = self.parent.toFormData(self.parent.formData);
 
-            axios.post(this.parent.url + "/site/login", data).then(function(response) {
+            axios.post(self.parent.url + "/site/login", data).then(function (response) {
                 if (response.data.error) {
-                    self.parent.$refs.msg.alertFun(response.data.error);
-                } else {
-                    self.parent.user = response.data;
-
-                    window.localStorage.setItem('user', JSON.stringify(self.parent.user));
-
-                    self.$router.push('/campaigns');
+                    if (self.$refs.msg) {
+                        self.$refs.msg.alertFun(response.data.error);
+                    } else {
+                        console.error("Помилка:", response.data.error);
+                        alert(response.data.error);
+                    }
                 }
-            }).catch(function(error) {
-                console.log(error);
+                if (response.data.user) {
+                    self.parent.user = response.data.user;
+                    self.parent.page('/campaigns');
+                    window.localStorage.setItem('user', JSON.stringify(self.parent.user));
+                }
+            }).catch(function (error) {
+                console.log('errors : ', error);
             });
         }
     }
